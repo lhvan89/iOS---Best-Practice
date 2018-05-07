6f57fa2c7bbf10021674f044f8ab637fd62c8189
lhvan89 <lhvan89@gmail.com>
2018-05-07 08:55:30 +0700

    taglistview

---
+++ TagListViewDemo/TagListViewDemo/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TagListViewDemo
+//
+//  Created by lhvan on 5/6/18.
+//  Copyright © 2018 lhvan. All rights reserved.
+//
+
+import UIKit
+import TagListView
+
+class ViewController: UIViewController, TagListViewDelegate {
+    @IBOutlet weak var tagList: TagListView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tagList.delegate = self
+        
+        tagList.addTag("Hello")
+        tagList.addTag("Welcome")
+        tagList.addTag("Book")
+        tagList.addTag("Tivi show")
+        tagList.addTag("Hello")
+        tagList.addTag("Welcome")
+        tagList.addTag("Book")
+        tagList.addTag("Tivi show")
+        tagList.addTag("Hello")
+        tagList.addTag("Welcome")
+        tagList.addTag("Book")
+        tagList.addTag("Tivi show")
+        tagList.addTag("Hello")
+        tagList.addTag("Welcome")
+        tagList.addTag("Book")
+        tagList.addTag("Tivi show")
+    }
+    
+    func tagPressed(_ title: String, tagView: TagView, sender: TagListView) {
+        tagView.isSelected = !tagView.isSelected
+    }
+    func tagRemoveButtonPressed(_ title: String, tagView: TagView, sender: TagListView) {
+        tagList.removeTagView(tagView)
+    }
+}
