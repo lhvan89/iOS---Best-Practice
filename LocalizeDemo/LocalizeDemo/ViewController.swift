ce626ae745b0f3553a4ac4cc492ad85228757adf
lhvan89 <lhvan89@gmail.com>
2018-05-07 11:05:11 +0700

    add Localize-Swift

---
+++ LocalizeDemo/LocalizeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalizeDemo
+//
+//  Created by Van on 5/7/18.
+//  Copyright © 2018 Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
