cdb4164f6f662dd826625415e70a06d46755e3c3
lhvan89 <lhvan89@gmail.com>
2018-05-09 14:56:46 +0700

    update

---
+++ NoticeSwift/NoticeSwift/SecondViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SecondViewController.swift
+//  NoticeSwift
+//
+//  Created by Van on 5/9/18.
+//  Copyright © 2018 Van. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pleaseWaite(_ sender: Any) {
+        self.pleaseWait()
+    }
+    
+    @IBAction func successNotice(_ sender: Any) {
+        self.successNotice("Success", autoClear: true)
+    }
+    
+    @IBAction func errorNotice(_ sender: Any) {
+        self.errorNotice("Error", autoClear: true)
+    }
+    
+    @IBAction func infoNotice(_ sender: Any) {
+        self.infoNotice("This is info notice", autoClear: true)
+    }
+    
+    @IBAction func noticeOnlyText(_ sender: Any) {
+        self.notice("SwiftNotice is a GUI library for displaying various popups (HUD) written in pure Swift, fits any scrollview and supports iPhone X.", type: .error, autoClear: true)
+    }
+    
+    @IBAction func noticeTop(_ sender: Any) {
+        self.noticeTop("insert successfull", autoClear: true, autoClearTime: 3)
+    }
+    
+    @IBAction func clearAllNotice(_ sender: Any) {
+        self.clearAllNotice()
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
