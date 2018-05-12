c76ddd9f2ccf54a4e43d9cd35c85b13fb144e008
lhvan89 <lhvan89@gmail.com>
2018-05-13 01:41:18 +0700

    Demo search city with RxSwift

--- RxSwift-Demo/RxSwift-Demo/ViewController.swift
+++ RxSwift-Demo/RxSwift-Demo/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    var shownCities = [String]() // Data source for UITableView
+    let allCities = ["Oklahoma", "Chicago", "Moscow", "Danang", "Vancouver", "Praga"] // Mocked API data source
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallocated
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        searchBar
+            .rx.text // Observable property
+            .orEmpty // Make it non-optional
+            .debounce(0.8, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
+            // .filter { !$0.isEmpty }
+            .subscribe(onNext: { [unowned self] query in // Here we will be notified of every new value
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities
+                print(self.shownCities)
+                self.tableView.reloadData() // And reload table view data
+            })
+            .disposed(by: disposeBag)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = shownCities[indexPath.row]
+        return cell!
+    }
+}
