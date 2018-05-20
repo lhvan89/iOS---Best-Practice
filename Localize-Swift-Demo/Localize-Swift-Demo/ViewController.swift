b7a28e212b18238304df6042978f74c12e11ea95
lhvan89 <lhvan89@gmail.com>
2018-05-20 23:11:20 +0700

    Start Project Localize-Swift

---
+++ Localize-Swift-Demo/Localize-Swift-Demo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Localize-Swift-Demo
+//
+//  Created by lhvan on 5/20/18.
+//  Copyright © 2018 lhvan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func doChangeLanguage(_ sender: Any) {
+    }
+}
+
