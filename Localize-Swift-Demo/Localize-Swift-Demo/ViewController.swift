5d83887bbdd89d1950474070fc9b216857e0f3a6
lhvan89 <lhvan89@gmail.com>
2018-05-20 23:57:26 +0700

    Final Localize Swift

--- Localize-Swift-Demo/Localize-Swift-Demo/ViewController.swift
+++ Localize-Swift-Demo/Localize-Swift-Demo/ViewController.swift
@@ -7,16 +7,42 @@
 //
 
 import UIKit
+import Localize_Swift
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
+    let availableLanguages = Localize.availableLanguages()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setText()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(setText), name: NSNotification.Name( LCLLanguageChangeNotification), object: nil)
+    }
+    
+    @objc func setText(){
+        label.text = "Hello".localized()
     }
 
     @IBAction func doChangeLanguage(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: "Switch Language", preferredStyle: UIAlertControllerStyle.actionSheet)
+        for language in availableLanguages {
+            let displayName = Localize.displayNameForLanguage(language)
+            let languageAction = UIAlertAction(title: displayName, style: .default, handler: {
+                (alert: UIAlertAction!) -> Void in
+                Localize.setCurrentLanguage(language)
+            })
+            actionSheet.addAction(languageAction)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: {
+            (alert: UIAlertAction) -> Void in
+        })
+        actionSheet.addAction(cancelAction)
+        self.present(actionSheet, animated: true, completion: nil)
     }
 }
 
