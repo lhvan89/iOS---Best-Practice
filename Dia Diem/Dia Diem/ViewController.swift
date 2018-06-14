9903fdc1cdbe59ea3814b94fb29fef093f584a19
Van Le <van.le@edge-works.net>
2018-06-14 08:31:34 +0700

    add địa

---
+++ Dia Diem/Dia Diem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dia Diem
+//
+//  Created by Van Le on 6/13/18.
+//  Copyright © 2018 Van Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
